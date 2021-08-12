014bb4663dc24871dfc1c2cba11a1f8c3290924c
Tobias Quante <t.quante@outlook.com>
2021-08-12 20:38:32 +0200

    feat: extend accordion with header

---
+++ vite-login-app/src/components/UI/Accordion/QAccordion.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="q-accordion-wrapper">
+    <header @click="togglePanels" class="q-accordion-header">
+      {{ header }}
+    </header>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    header: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    togglePanels(ev) {
+      const panelNodes =
+        ev.target.parentElement.querySelectorAll(".q-accordion-body");
+
+      panelNodes.forEach((panel) => {
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style>
+.q-accordion-wrapper {
+  color: var(--text-color-primary);
+  width: 100%;
+}
+
+.q-accordion-header {
+  width: 100%;
+  background-color: var(--background-color-secondary);
+  padding: var(--gap-lg);
+  font-size: var(--text-size-xxl);
+  color: var(--text-color-primary);
+  text-align: left;
+  border: none;
+  cursor: pointer;
+}
+</style>
---
+++ vite-login-app/src/components/UI/Accordion/QAccordionItem.vue
@@ -0,0 +1,66 @@
+<template>
+  <section class="q-accordion-body">
+    <slot />
+  </section>
+  <button @click.native="togglePanel" class="q-accordion-button">
+    {{ title }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    key: {
+      type: String,
+      required: false,
+      default: 0,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+
+  methods: {
+    togglePanel(ev) {
+      const panel = ev.target.previousElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.q-accordion-button {
+  width: 100%;
+  background-color: var(--background-color-secondary);
+  padding: var(--gap-lg);
+  font-size: var(--text-size-xl);
+  color: var(--text-color-primary);
+  text-align: left;
+  border: none;
+  cursor: pointer;
+}
+
+.q-accordion-button:nth-child(1) {
+  border-top-left-radius: var(--gap-xs);
+  border-top-right-radius: var(--gap-xs);
+}
+
+.q-accordion-button:last-child {
+  border-bottom-left-radius: var(--gap-xs);
+  border-bottom-right-radius: var(--gap-xs);
+}
+
+.q-accordion-body {
+  background-color: var(--background-color-tartiary);
+  max-height: 0;
+  overflow: hidden;
+  transition: max-height var(--duration-quickest) ease-in-out;
+  padding: 0 var(--gap-lg);
+}
+</style>
